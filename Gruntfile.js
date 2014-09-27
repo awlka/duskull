8d34e2904276adfcb4eebc7c3122a78de69a399b
Lucas Churchill <lucas@agtlucas.com>
2014-09-27 09:49:54 -0300

    Updates on Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,27 @@
 
 module.exports = function (grunt) {
 
+  // Show elapsed time after tasks run
+  require('time-grunt')(grunt);
   // Load all Grunt tasks
   require('load-grunt-tasks')(grunt);
 
   // Project configuration
+  grunt.initConfig({
 
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Uglify!
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'assets/js/**/*.js',
+        dest: 'build/js/main.'
+      }
+    }
+
+  });
 
 };
